c530820a43f69803a35078271fd7857fc33a6e86
soybean <1242061448@qq.com>
2023-11-15 16:29:40 +0800

    提交ServerSocketChannel

---
+++ Redis-Mongodb-demo/src/test/java/com/syb/MongodbApplicationTests.java
@@ -0,0 +1,38 @@
+package com.syb;
+
+import com.syb.pojo.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.BasicQuery;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
+
+/**
+ * @Author: sun
+ * @Date: 2023/11/15/15:54
+ */
+@SpringBootTest
+public class MongodbApplicationTests {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    @Test
+    void contextLoads() {
+        Book book = new Book();
+        book.setId(2);
+        book.setName("springboot2");
+        book.setType("springboot2");
+        book.setDescription("springboot2");
+        mongoTemplate.save(book);
+    }
+
+    //查询所有
+    @Test
+    void findAll() {
+        List<Book> books = mongoTemplate.findAll(Book.class);
+        System.out.println(books);
+    }
+
+}
